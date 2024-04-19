60d9c9d8d11eb57eaf9dbee561ae49919a2d42a0
dileep <dileepreddy0503@gmail.com>
2024-04-19 13:16:38 +0530

    Added Catalog config changes

--- catalog-service/src/test/java/com/bookstore/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/bookstore/CatalogServiceApplicationTests.java
@@ -2,9 +2,12 @@ package com.bookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(ContainerConfig.class)
 class CatalogServiceApplicationTests {
+
 
 	@Test
 	void contextLoads() {
